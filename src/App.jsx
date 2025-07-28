b5e49806350da0be593725d8f65e7659cbd6d0fa
Your Name <you@example.com>
2025-07-28 15:41:09 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename="/Jobby-App">
+    <BrowserRouter basename="/">
       <Routes>
         <Route 
           path="/login" 
